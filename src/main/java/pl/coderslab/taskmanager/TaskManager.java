a6bf30a6107f6afcb278327c7b452077db20d9f4
FilipMichal <michal.s.filip@gmail.com>
2020-09-07 20:10:05 +0200

    pierwszy warsztat

--- src/main/java/pl/coderslab/taskmanager/TaskManager.java
+++ src/main/java/pl/coderslab/taskmanager/TaskManager.java
@@ -1,8 +1,7 @@
 package pl.coderslab.taskmanager;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -10,7 +9,7 @@ import java.util.Scanner;
 
 public class TaskManager {
 
-    private static String[][] taskList = new String[1][];
+    private static String[][] taskList = new String[0][];
 
     public static void main(String[] args) {
         TaskManager.run();
@@ -37,24 +36,38 @@ public class TaskManager {
     }
 
     private static void saveTaskListToFile() {
+        String fileName = "tasks.csv";
+        StringBuilder nextItem = new StringBuilder();
+        int count = 0;
+        for (String[] task : taskList) {
+            nextItem.append(taskList[count][0]).append(", ")
+                    .append(taskList[count][1]).append(", ")
+                    .append(taskList[count][2]).append("\n");
+            count++;
+            try (FileWriter nextOutput = new FileWriter(fileName, false)) {
+                nextOutput.append(nextItem.toString());
 
+            } catch (IOException ex) {
+                System.out.println("Writing to file error.");
+            }
+        }
     }
 
     private static void loadTaskListFromFile() {
         String fileName = "tasks.csv";
-        Path taskFilePath = Paths.get(fileName);
+//        Path taskFilePath = Paths.get(fileName);
         File tasksFile = new File(fileName);
         int count = 0;
         try {
-            Scanner scan = new Scanner(taskFilePath);
+            Scanner scan = new Scanner(tasksFile);
             while (scan.hasNextLine()) {
                 String content = scan.nextLine();
                 taskList = Arrays.copyOf(taskList, count + 1);
                 taskList[count] = content.split(", ");
                 count++;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 //        for (int i = 0; i < taskList.length; i++) {
 //            System.out.println(Arrays.toString(taskList[i]));
@@ -81,33 +94,62 @@ public class TaskManager {
                 executeAddChoice();
                 break;
             case "remove" :
-                //obsługa
                 executeRemoveChoice();
                 break;
             case "list" :
-                //obsługa
                 executeListChoice();
                 break;
         }
     }
 
     private static void executeListChoice() {
+        StringBuilder taskContent = new StringBuilder();
+        int count = 0;
+        for (String[] task : taskList) {
+            taskContent.append(count + " :\t").append(task[0] +", ").append(task[1] + ", ").append(task[2] + "\n");
+            count++;
+        }
+        System.out.println(taskContent.toString());
+
     }
 
     private static void executeRemoveChoice() {
+        Scanner toRemove = new Scanner(System.in);
+        System.out.println("Select the task to remove. Enter number from 0 to " + (taskList.length - 1) + " :" );
+        while (!toRemove.hasNextInt()) {
+            toRemove.nextLine();
+            System.out.println("Enter valid value: 0 - " + (taskList.length - 1));
+        }
+        int userSelected = toRemove.nextInt();
+        if (userSelected < 0 || userSelected >= taskList.length) {
+            System.out.println("Entered value is invalid, enter again: ");
+        } else if (userSelected == taskList.length - 1) {
+            taskList = Arrays.copyOf(taskList, taskList.length - 1);
+        } else if (userSelected >= 0 && userSelected < taskList.length - 1){
+            for (int i = userSelected; i < taskList.length - 1; i++) {
+                taskList[i] = taskList[i + 1];
+            }
+            taskList = Arrays.copyOf(taskList, taskList.length - 1);
+        }
+
     }
 
     private static void executeAddChoice() {
         Scanner scan = new Scanner(System.in);
-        System.out.println(ConsoleColors.BLUE + "Podaj opis zadania: ");
+        System.out.println(ConsoleColors.BLUE + "Enter task description: ");
         System.out.print(ConsoleColors.RESET);
         String description = scan.nextLine();
-        System.out.println(ConsoleColors.BLUE + "Podaj termin wykonania (rrrr-mm-dd): ");
+        System.out.println(ConsoleColors.BLUE + "Enter the due date (yyyy-mm-dd): ");
         System.out.print(ConsoleColors.RESET);
         String dueDate = scan.nextLine();
-        System.out.println(ConsoleColors.BLUE + "Czy to ważne zadanie (true/false): ");
+        System.out.println(ConsoleColors.BLUE + "Is this task important (true/false): ");
         System.out.print(ConsoleColors.RESET);
         String priority = scan.nextLine();
+        StringBuilder toAdd = new StringBuilder();
+        toAdd.append(description).append(", ").append(dueDate).append(", ").append(priority);
+//        String newTask = toAdd.toString();
+        taskList = Arrays.copyOf(taskList, taskList.length + 1);
+        taskList[taskList.length - 1] = toAdd.toString().split(", ");
     }
 
     private static boolean isExitChoice(String userChoice) {
